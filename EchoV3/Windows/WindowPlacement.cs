a132ed6d98368c4bf374c8b957ae0bfd7fa2d49b
Richard Yawger <rjyawger@me.com>
2023-07-09 23:11:34 -0400

    preserve window placement

---
+++ EchoV3/Windows/WindowPlacement.cs
@@ -0,0 +1,106 @@
+﻿
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows;
+using System.Windows.Interop;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace EchoV3.Windows
+{
+    internal static class WindowExtensions
+    {
+        public static void SetPlacement(this Window window, WINDOWPLACEMENT placement)
+        {
+            WindowPlacement.SetPlacement(new WindowInteropHelper(window).Handle, placement);
+        }
+
+        public static WINDOWPLACEMENT GetPlacement(this Window window)
+        {
+            return WindowPlacement.GetPlacement(new WindowInteropHelper(window).Handle);
+        }
+    }
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RECT
+    {
+        public int Left;
+        public int Top;
+        public int Right;
+        public int Bottom;
+
+        public RECT(int left, int top, int right, int bottom)
+        {
+            this.Left = left;
+            this.Top = top;
+            this.Right = right;
+            this.Bottom = bottom;
+        }
+    }
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct POINT
+    {
+        public int X;
+        public int Y;
+
+        public POINT(int x, int y)
+        {
+            this.X = x;
+            this.Y = y;
+        }
+    }
+
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential)]
+    public struct WINDOWPLACEMENT
+    {
+        public int length;
+        public int flags;
+        public int showCmd;
+        public POINT minPosition;
+        public POINT maxPosition;
+        public RECT normalPosition;
+    }
+
+    public static class WindowPlacement
+    {
+        private static Encoding encoding = new UTF8Encoding();
+        private static XmlSerializer serializer = new XmlSerializer(typeof(WINDOWPLACEMENT));
+
+        [DllImport("user32.dll")]
+        private static extern bool SetWindowPlacement(IntPtr hWnd, [In] ref WINDOWPLACEMENT lpwndpl);
+
+        [DllImport("user32.dll")]
+        private static extern bool GetWindowPlacement(IntPtr hWnd, out WINDOWPLACEMENT lpwndpl);
+
+        private const int SW_SHOWNORMAL = 1;
+        private const int SW_SHOWMINIMIZED = 2;
+
+        public static void SetPlacement(IntPtr windowHandle, WINDOWPLACEMENT placement)
+        {
+            try
+            {
+                placement.length = Marshal.SizeOf(typeof(WINDOWPLACEMENT));
+                placement.flags = 0;
+                placement.showCmd = (placement.showCmd == SW_SHOWMINIMIZED ? SW_SHOWNORMAL : placement.showCmd);
+                SetWindowPlacement(windowHandle, ref placement);
+            }
+            catch (InvalidOperationException)
+            {
+                // Parsing placement failed. Fail silently.
+            }
+        }
+
+        public static WINDOWPLACEMENT GetPlacement(IntPtr windowHandle)
+        {
+            WINDOWPLACEMENT placement = new WINDOWPLACEMENT();
+            GetWindowPlacement(windowHandle, out placement);
+            return placement;
+        }
+    }
+}
